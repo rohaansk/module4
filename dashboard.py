75ad620351f90d631d9bf8cbcfa71c1ab5c27160
rohaansk <103877616+rohaansk@users.noreply.github.com>
2025-03-16 19:21:37 -0400

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -85,7 +85,7 @@ if not df_filtered.empty:
     kpi_options = ["Sales", "Quantity", "Profit", "Margin Rate"]
     selected_kpi = st.radio("Select KPI to visualize:", options=kpi_options, horizontal=True)
     
-    col1, col2 = st.columns([1, 1])
+    col1, empty_col, col2 = st.columns([0.8, 0.4, 0.8])  # Adds space between charts
     with col1:
         fig_line = px.line(df_grouped, x="Order Date", y=selected_kpi, title=f"{selected_kpi} Over Time",
                            labels={"Order Date": "Date", selected_kpi: selected_kpi}, template="plotly_white",
